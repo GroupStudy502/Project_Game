1d5077450b9f018da42379341507c1110ce04757
0taelove <0taelove@gmailc.com>
2024-05-28 19:33:34 +0900

    게임 기능

--- src/main/java/org/choongang/global/AbstractController.java
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -1,6 +1,6 @@
 package org.choongang.global;
 
-import org.choongang.global.constants.Menu;
+import org.choongang.global.constants.MainMenu;
 import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
 
@@ -25,8 +25,8 @@ public abstract class AbstractController implements Controller {
 
     /**
      * 입력 항목
-     * - 문자: q, exit, quit - 종료
-     * - 숫자: 메뉴 항목
+     *  - 문자: q, exit, quit - 종료
+     *  - 숫자: 메뉴 항목
      */
     public void prompt() {
         System.out.print("메뉴 선택: ");
@@ -48,20 +48,18 @@ public abstract class AbstractController implements Controller {
     /**
      * 입력과 검증을 함께 진행
      *
-     * @param message   : 항목 메세지
+     * @param message : 항목 메세지
      * @param predicate : 판별식
      * @return
      */
-
     protected String promptWithValidation(String message, Predicate<String> predicate) {
         String str = null;
         do {
             System.out.print(message);
             str = sc.nextLine();
-        } while (!predicate.test(str));
+        } while(!predicate.test(str));
 
         return str;
-
     }
 
     /**
@@ -76,14 +74,15 @@ public abstract class AbstractController implements Controller {
     }
 
     private void change(int menuNo) {
-        Menu menu = null;
+        MainMenu mainMenu = null;
         switch(menuNo) {
-            case 1: menu = Menu.JOIN; break; // 회원가입
-            case 2: menu = Menu.LOGIN; break; // 로그인
-            default: menu = Menu.MAIN; // 메인 메뉴
+            case 1: mainMenu = MainMenu.JOIN; break; // 회원가입
+            case 2: mainMenu = MainMenu.LOGIN; break; // 로그인
+            case 3: mainMenu = MainMenu.GAME; break; // 게임하기
+            default: mainMenu = MainMenu.MAIN; // 메인 메뉴
         }
 
         // 메뉴 컨트롤러 변경 처리 - Router
-        MainRouter.getInstance().change(menu);
+        MainRouter.getInstance().change(mainMenu);
     }
 }
--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -1,7 +1,7 @@
 package org.choongang.main.controllers;
 
 import org.choongang.global.AbstractController;
-import org.choongang.global.constants.Menu;
+import org.choongang.global.constants.MainMenu;
 import org.choongang.template.Templates;
 
 /**
@@ -12,6 +12,7 @@ public class MainController extends AbstractController {
     @Override
     public void show() {
 
-        Templates.getInstance().render(Menu.MAIN);
+
+        Templates.getInstance().render(MainMenu.MAIN);
     }
 }
