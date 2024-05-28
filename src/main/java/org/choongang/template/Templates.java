6e8a7e5ebab7cbb1f9e1beb25f14831de5316fb3
0taelove <0taelove@gmailc.com>
2024-05-28 20:43:58 +0900

    게임 기능

--- src/main/java/org/choongang/template/Templates.java
+++ src/main/java/org/choongang/template/Templates.java
@@ -1,7 +1,10 @@
 package org.choongang.template;
 
+import org.choongang.game.constants.SubMenu;
+import org.choongang.global.Menu;
 import org.choongang.global.constants.MainMenu;
 import org.choongang.template.game.GameTpl;
+import org.choongang.template.game.RankTpl;
 import org.choongang.template.main.MainTpl;
 import org.choongang.template.member.JoinTpl;
 import org.choongang.template.member.LoginTpl;
@@ -9,10 +12,11 @@ import org.choongang.template.member.MypageTpl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class Templates {
     private static Templates instance;
-    private Map<MainMenu, Template> tpls;
+    private Map<Menu, Template> tpls;
 
     private Templates() {
         tpls = new HashMap<>();
@@ -26,26 +30,52 @@ public class Templates {
         return instance;
     }
 
-    public void render(MainMenu mainMenu) {
-
-        System.out.println(find(mainMenu).getTpl());
+    public void render(Menu menu) {
+        render(menu, null);
     }
 
-    public Template find(MainMenu mainMenu) {
-        Template tpl = tpls.get(mainMenu);
+    public void render(Menu menu, Supplier<String> hook) {
+
+        System.out.println(find(menu, hook).getTpl());
+    }
+
+    public Template find(Menu menu, Supplier<String> hook) {
+        Template tpl = tpls.get(menu);
         if (tpl != null) {
             return tpl;
         }
-
-        switch (mainMenu) {
-            case JOIN: tpl = new JoinTpl(); break;
-            case LOGIN: tpl = new LoginTpl(); break;
-            case MYPAGE: tpl = new MypageTpl(); break;
-            case GAME: tpl = new GameTpl(); break;
-            default: tpl = new MainTpl();
+        if (menu instanceof SubMenu) {
+            SubMenu subMenu = (SubMenu) menu;
+            switch(subMenu) {
+                case ALONE:
+                case TOGETHER:
+                case RANKING: tpl = new RankTpl(); break;
+            }
+        } else {
+            MainMenu mainMenu = (MainMenu)menu;
+            switch (mainMenu) {
+                case JOIN:
+                    tpl = new JoinTpl();
+                    break;
+                case LOGIN:
+                    tpl = new LoginTpl();
+                    break;
+                case MYPAGE:
+                    tpl = new MypageTpl();
+                    break;
+                case GAME:
+                    tpl = new GameTpl();
+                    break;
+                default:
+                    tpl = new MainTpl();
+            }
         }
 
-        tpls.put(mainMenu, tpl);
+        if (hook != null) {
+            tpl.addHook(hook);
+        }
+
+        tpls.put(menu, tpl);
 
         return tpl;
     }
