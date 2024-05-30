76aa6b417dd6424713ba13d71e07f4a73f7fdac7
decl5th <alsgur6624@gmail.com>
2024-05-30 20:25:47 +0900

    socket 관련

--- src/main/java/org/choongang/game/controllers/GameControllerLocator.java
+++ src/main/java/org/choongang/game/controllers/GameControllerLocator.java
@@ -37,8 +37,8 @@ public class GameControllerLocator extends AbstractControllerLocator {
                 case RANKING: controller = new RankingController(); break;
             }
         } else { // 게임 메인 메뉴
-            //controller = new GameController();
-            controller = new PlayController();
+            controller = new GameController();
+            //controller = new PlayController();
         }
 
 
